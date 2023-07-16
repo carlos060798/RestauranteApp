0336aae569f27b1ed8b792b406d8c878ec9ebbd7
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-07-16 14:08:08 -0500

    version final backend

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,4 +1,5 @@
 // reservasController.js
+const bcrypt = require('bcrypt');
 
 const db = require("../db");
      
@@ -50,81 +51,30 @@ exports.listarUsers = (req, res) => {
         .json({ message: "usuarios listados exitosamente.", result });
     });
 }
+exports.AutenticarUser = (req, res) => {
+  const { email, password } = req.body;
 
-exports.AutenticarUser = async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      // Verificar si el usuario existe en la base de datos
-      db.query('SELECT * FROM usuarios WHERE email = ? AND password = ?', [email, password], (err, result) => {
-        if (err) {
-          console.error('Error al realizar la autenticación', err);
-          res.status(500).json({ message: 'Error en el servidor' });
-        } else {
-          if (result.length === 0) {
-            res.status(404).json({ message: 'Usuario no encontrado' });
-          } else {
-            // Verificar la contraseña ingresada con la almacenada en la base de datos
-            if (password !== result[0].password) {
-              res.status(401).json({ message: 'Contraseña incorrecta' });
-            } else {
-              // Autenticación exitosa
-              res.json({ message: 'Autenticación exitosa' });
-            }
-          }
-        }
-      });
-    } catch (error) {
-      console.error('Error al realizar la autenticación', error);
-      res.status(500).json({ message: 'Error en el servidor' });
+  // Verificar si el usuario existe en la base de datos
+  db.query('SELECT * FROM usuarios WHERE email = ?', [email], (err, results) => {
+    if (err) {
+      console.error('Error al realizar la consulta', err);
+      return res.status(500).json({ message: 'Error en el servidor' });
     }
-  };
 
-
-
-
-  /*
-  
-
-exports.listarUsers = (req, res) => {
-    const query = "SELECT * FROM usuarios";
-    db.query(query, (error, result) => {
-      if (error) {
-        console.error("Error al listar usuarios", error);
-        return res
-          .status(500)
-          .json({ message: "Ocurrió un error al listar los usuarios." });
-      }
-  
-      // La reserva se insertó correctamente
-      return res
-        .status(200)
-        .json({ message: "usuarios listados exitosamente.", result });
-    });
-}
-/*exports.AutenticarUser = async (req, res) => {
-    const { email, password } = req.body;
-     console.log(email,password);
-  
-    try {
-      // Verificar si el usuario existe en la base de datos
-      const user = await db.query('SELECT * FROM usuarios WHERE email = ? AND password = ?', [email, password]);  
-     console.log(user);
-      if (email!== user[0].email) {
-        return res.status(404).json({ message: 'Usuario no encontrado' });
-      }
-  
-      // Verificar la contraseña ingresada con la almacenada en la base de datos
-      if (password!== user[1].password) {
-        return res.status(401).json({ message: 'Contraseña incorrecta' });
-      }
-  
-      // Autenticación exitosa
-      res.json({ message: 'Autenticación exitosa' });
-    } catch (error) {
-      console.error('Error al realizar la autenticación', error);
-      res.status(500).json({ message: 'Error en el servidor' });
+    // Verificar si el correo existe en la base de datos
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Correo no encontrado' });
     }
-  };
 
-*/
+    // Verificar la contraseña ingresada con la almacenada en la base de datos
+    if (password !== results[0].password) {
+      return res.status(401).json({ message: 'Contraseña incorrecta' });
+    }
+
+    // Autenticación exitosa
+    res.json({ message: 'Autenticación exitosa' });
+  });
+};
+
+  
+
