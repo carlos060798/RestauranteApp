d4f88be76e85cc8c56d5f21874d320ab249c0a46
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-07-19 10:09:45 -0500

    version fianl  reservas

--- frontend/src/Componets/generales/FormReserva.jsx
+++ frontend/src/Componets/generales/FormReserva.jsx
@@ -1,108 +1,13 @@
-import { useState } from "react";
-import axios from "axios";
 import Alerta from "./Alerta"; // Importamos el componente de alerta
+import useResevas from "../../hooks/useReservas,";
 
 function FormReservas() {
-  const [formData, setFormData] = useState({
-    nombres: "",
-    apellidos: "",
-    tipodocumento: "",
-    identificacion: "",
-    email: "",
-  });
-
-  const [DataReserva, setDataReserva] = useState({
-    fecha_reserva: "",
-    tipo_reserva: "",
-    cantidad_personas: "",
-    observaciones: "",
-  });
-
-  const [alerta, setAlerta] = useState({}); // Estado para mostrar la alerta
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
-
-  const handleChangeReserva = (e) => {
-    const { name, value } = e.target;
-    setDataReserva((prevDataReserva) => ({
-      ...prevDataReserva,
-      [name]: value,
-    }));
-  };
-
-  const handleReserva = async (e) => {
-    e.preventDefault();
-
-    // Validar que no falten campos
-    if (
-      [
-        formData.nombres,
-        formData.apellidos,
-        formData.tipodocumento,
-        formData.identificacion,
-        formData.email,
-        DataReserva.fecha_reserva,
-        DataReserva.tipo_reserva,
-        DataReserva.cantidad_personas,
-      ].includes("")
-    ) {
-      setAlerta({
-        msg: "Todos los campos son obligatorios",
-        error: true,
-      });
-      return;
-    }
-
-    try {
-      // 1. Llamar a la API para crear el usuario
-      const usuarioResponse = await axios.post(
-        "http://localhost:4000/user",
-        formData
-      );
-      const usuarioId = usuarioResponse.data.idUsuer;
-
-      // 2. Llamar a la API para crear la reserva usando el ID del usuario
-      await axios.post(
-        `http://localhost:4000/reservas/${usuarioId}`,
-        DataReserva
-      );
-
-      console.log("Reserva creada exitosamente");
-      // Mostrar alerta de éxito
-      setAlerta({
-        msg: "Reserva creada exitosamente",
-        error: false,
-      });
-
-      // Limpiar formulario
-      setFormData({
-        nombres: "",
-        apellidos: "",
-        tipodocumento: "",
-        identificacion: "",
-        email: "",
-      });
-      setDataReserva({
-        fecha_reserva: "",
-        tipo_reserva: "",
-        cantidad_personas: "",
-        observaciones: "",
-      });
-    } catch (error) {
-      console.error("Error al crear la reserva:", error);
-      // Mostrar alerta de error
-      setAlerta({
-        msg: "Error al crear la reserva",
-        error: true,
-      });
-    }
-  };
+  const { handleReserva,
+    handleChange,
+    handleChangeReserva,
+    formData,
+    DataReserva,
+    alerta } = useResevas();
 
   return (
     <>
